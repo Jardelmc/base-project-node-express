0c33cdc84a671392824d43e170a4c20ee092662d
Jardel <jardel.casteluber@gmail.com>
2020-05-27 23:28:44 -0300

    update dependencies

---
+++ packageUpdate.sh
@@ -0,0 +1,1 @@
+yarn add bcryptjs cors dotenv express jsonwebtoken mongoose multer yup && yarn add eslint eslint-config-airbnb-base eslint-config-prettier eslint-plugin-import eslint-plugin-prettier nodemon prettier sucrase --dev
